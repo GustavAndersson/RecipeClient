b4282beff2b05c5606b9e0ef9f43f180f9bd0ced
GustavAndersson <guustav.andersson@gmail.com>
2017-01-19 16:40:51 +0100

    Added login
    You can now login or register a new user

--- public_html/js/recipeApp.js
+++ public_html/js/recipeApp.js
@@ -7,8 +7,11 @@ module.config(function ($urlRouterProvider, $stateProvider) {
         url: "/",
         templateUrl: "templates/home.html",
         controller: "homeCtrl"
+    }).state("recipe", {
+        url:"/recipe",
+        templateUrl: "templates/recipe.html",
+        controller:"recipeCtrl"
     });
-    
 });
 
 module.controller("homeCtrl", function ($scope, recipeService) {
@@ -20,15 +23,59 @@ module.controller("homeCtrl", function ($scope, recipeService) {
     });
 });
 
-module.service("recipeService", function ($q, $http) {
+module.controller("loggInCtrl", function ($scope, recipeService){
+    $scope.createUser = function () {
+        recipeService.createUser($scope.name, $scope.password);
+    };
+});
+
+module.service("recipeService", function ($q, $rootScope, $http) {
     this.getTable = function () {
         var deffer = $q.defer();
-        var url = "http://localhost:8080/RecipeApp/webresources/recipes";
+        var url = "http://localhost:8080/RecipeApp/webresources/viewRecipes";
         $http.get(url).then(function (data) {
             deffer.resolve(data);
         });
         return deffer.promise;
     };
-});
+    
+    this.createUser = function (name, password) {
+        var data = {
+            name: name,
+            password: password
+        };
+        var url = "http://localhost:8080/RecipeApp/webresources/createUser";
 
+        $http({
+            url: url,
+            method: "POST",
+            data: data
+        }).then(function (data) {
+            console.log("Användare tillagd");
+        },function (data){
+            console.log("Användare med detta namnet finns redan, loggar in istället");
+            loggIn(name, password);
+        });
+    };
+    
+    loggIn = function (username, password) {
+        var url = "http://localhost:8080/RecipeApp/webresources/login";
+        var auth = "Basic " + window.btoa(username + ":" + password);
+
+        $http({
+            url: url,
+            method: "POST",
+            headers: {'Authorization': auth}
+        }).then(function (data) {
+            console.log("Du är inloggad!");
+            $rootScope.isLoggedIn = true;
+            $rootScope.user = username;
+            $rootScope.pass = password;
+        }, function(data) {
+            alert("Wrong password");
+            console.log("Du kan inte logga in");
+        });
+    };
+    
+});
 
