d2f0404c84151eba5d61f3783be1dfcde32c5d22
GustavAndersson <guustav.andersson@gmail.com>
2017-01-23 18:54:48 +0100

    AddRecipe works

--- public_html/js/recipeApp.js
+++ public_html/js/recipeApp.js
@@ -11,6 +11,10 @@ module.config(function ($urlRouterProvider, $stateProvider) {
         url:"/recipe/:id",
         templateUrl: "templates/recipe.html",
         controller:"recipeCtrl"
+    }).state("addRecipe", {
+        url:"/addRecipe",
+        templateUrl: "templates/addRecipe.html",
+        controller:"addRecipeCtrl"
     });
 });
 
@@ -29,6 +33,12 @@ module.controller("loggInCtrl", function ($scope, recipeService){
     };
 });
 
+module.controller("addRecipeCtrl", function ($scope, recipeService){
+    $scope.addRecipe = function () {
+        recipeService.addRecipe($scope.name, $scope.category, $scope.description, $scope.instruction, $scope.picture);
+    };
+});
+
 module.controller("recipeCtrl", function ($scope, $stateParams, recipeService){
     var id = $stateParams;
     var promise = recipeService.getRecipe(id.id);
@@ -39,6 +49,11 @@ module.controller("recipeCtrl", function ($scope, $stateParams, recipeService){
         $scope.description = data.data[0].description;
         $scope.instruction = data.data[0].instruction;
         $scope.picture = data.data[0].picture;
+    });
+    var promiseIng = recipeService.getRecipeIngredients(id.id);
+    promiseIng.then(function(data){
+        console.log(data.data);
+        $scope.table = data.data;
     });
 });
 
@@ -55,6 +70,15 @@ module.service("recipeService", function ($q, $rootScope, $http) {
     this.getRecipe = function(id){
         var deffer = $q.defer();
         var url = "http://localhost:8080/RecipeApp/webresources/recipe/" + id;
+        $http.get(url).then(function(data){
+            deffer.resolve(data);
+        });
+        return deffer.promise;
+    };
+    
+    this.getRecipeIngredients = function(id){
+        var deffer = $q.defer();
+        var url = "http://localhost:8080/RecipeApp/webresources/ingredients/" + id;
         $http.get(url).then(function(data){
             deffer.resolve(data);
         });
@@ -94,11 +118,35 @@ module.service("recipeService", function ($q, $rootScope, $http) {
             $rootScope.isLoggedIn = true;
             $rootScope.user = username;
             $rootScope.pass = password;
+            console.log(username, password);
         }, function(data) {
             alert("Sorry, wrong password. Try again or create a new user");
             console.log("Du kan inte logga in");
         });
+    };  
+    
+    this.addRecipe = function (name, category, description, instruction, picture) {
+        var data = {
+            name: name,
+            categori_id: category,
+            description: description,
+            instruction: instruction,
+            picture: picture
+        };
+        var url = "http://localhost:8080/RecipeApp/webresources/recipe";
+        var auth = "Basic " + window.btoa($rootScope.user + ":" + $rootScope.pass);
+
+        $http({
+            url: url,
+            method: "POST",
+            data: data,
+            headers: {'Authorization': auth}
+        }).then(function (data) {
+            console.log("Recept tillagt");
+            alert("Du har lagt till: " + name);
+        },function (data){
+            console.log("Det gick inte att lägga till receptet");
+        });
     };
     
 });
-
